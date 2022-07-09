34a112e5bbafadff342f825f6ea6f17e6d8fad68
blank2121 <70784842+blank2121@users.noreply.github.com>
2022-07-09 18:41:22 -0400

    should now work offline
    i fixed the service worker to now cache the website when offline

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,38 @@
-"use strict";
-// Cache Name
 const CACHE_NAME = "static-cache-v1";
 // Cache Files
-const FILES_TO_CACHE = ["/index.html"];
-// install
-self.addEventListener("install", (evt) => {
-  console.log("[ServiceWorker] Install");
+const FILES_TO_CACHE = ["/pwa/index.html", "/build/bundle.js", "/build/bundle.js.map", "/global.css", "https://fonts.googleapis.com/css2?family=Poppins&family=Source+Code+Pro:wght@300&display=swap"];
+
+self.addEventListener("install", evt => {
+  console.log("Service Worker: Installed");
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
-    })
+      caches
+      .open(CACHE_NAME)
+      .then(cache => {
+          console.log("Service Worker: Caching files");
+          cache.addAll(FILES_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
-});
-// Active PWA Cache and clear out anything older
-self.addEventListener("activate", (evt) => {
-  console.log("[ServiceWorker] Activate");
+})
+
+self.addEventListener("activate", evt => {
+  console.log("Service Worker: Activated");
+  //removing unwanted cache entries
   evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
+      caches.keys().then(CACHE_NAME => {
+          return Promise.all(
+              CACHE_NAME.map(cache => {
+                  if(cache === CACHE_NAME) {
+                      console.log("Service Worker: Clearing cache");
+                      return caches.delete(cache);
+                  }
+              })
+          )
+      })
   );
-  self.clients.claim();
 });
-// listen for fetch events in page navigation and return anything that has been cached
-self.addEventListener("fetch", (evt) => {
-  console.log("[ServiceWorker] Fetch", evt.request.url);
-  // when not a navigation event return
-  if (evt.request.mode !== "navigate") {
-    return;
-  }
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("index.html");
-      });
-    })
-  );
+
+self.addEventListener("fetch", evt => {
+  console.log('fetching...');
+  evt.respondWith(fetch(evt.request).catch(() => caches.match(evt.request)));
 });
