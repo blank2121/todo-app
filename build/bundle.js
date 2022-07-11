4cc398c08b935c63f5945d1c69d07082df2a97e4
blank2121 <70784842+blank2121@users.noreply.github.com>
2022-07-10 20:01:37 -0400

    removed service worker.js

--- build/bundle.js
+++ build/bundle.js
@@ -1,4 +1,3 @@
-navigator.serviceWorker.register('./service-worker.js');
 (function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
 var app = (function () {
     'use strict';
